d19d9be0ef5c127c141059f5929f966c259b5d19
Nathandev254 <warimwenathaniel@gmail.com>
2025-02-17 23:18:48 +0300

    today

--- server/index.ts
+++ server/index.ts
@@ -14,17 +14,5 @@ class LinkedList {
     this.head = null;
   }
 
-  // Add a node to the linked list
-  add(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
+ 
 
